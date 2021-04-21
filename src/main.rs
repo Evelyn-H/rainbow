54c2aaeb4ea44f269033e6c6d40a873c4caa2722
Evelyn-H <hobert.evelyn@gmail.com>
2021-04-20 21:44:15 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,314 @@
+use std::iter;
+use wgpu::util::DeviceExt;
+
+
+use winit::{
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::{Window, WindowBuilder},
+};
+
+// We need this for Rust to store our data correctly for the shaders
+#[repr(C)]
+// This is so we can store this in a buffer
+#[derive(Debug, Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
+struct Uniforms {
+    width: u32,
+    height: u32,
+    time: f32,
+}
+
+impl Uniforms {
+    fn new(width: u32, height: u32) -> Self {
+        Self {
+            width,
+            height,
+            time: 0.0,
+        }
+    }
+}
+
+
+struct State {
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    sc_desc: wgpu::SwapChainDescriptor,
+    swap_chain: wgpu::SwapChain,
+    size: winit::dpi::PhysicalSize<u32>,
+    render_pipeline: wgpu::RenderPipeline,
+    uniforms: Uniforms,
+    uniform_buffer: wgpu::Buffer,
+    uniform_bind_group: wgpu::BindGroup,
+}
+
+impl State {
+    async fn new(window: &Window) -> Self {
+        let size = window.inner_size();
+
+        // The instance is a handle to our GPU
+        // BackendBit::PRIMARY => Vulkan + Metal + DX12 + Browser WebGPU
+        let instance = wgpu::Instance::new(wgpu::BackendBit::PRIMARY);
+        let surface = unsafe { instance.create_surface(window) };
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                // power_preference: wgpu::PowerPreference::default(),
+                power_preference: wgpu::PowerPreference::HighPerformance,
+                compatible_surface: Some(&surface),
+            })
+            .await
+            .unwrap();
+
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    label: None,
+                    features: wgpu::Features::empty(),
+                    limits: wgpu::Limits::default(),
+                },
+                None, // Trace path
+            )
+            .await
+            .unwrap();
+
+        let sc_desc = wgpu::SwapChainDescriptor {
+            usage: wgpu::TextureUsage::RENDER_ATTACHMENT,
+            format: wgpu::TextureFormat::Bgra8UnormSrgb,
+            width: size.width,
+            height: size.height,
+            // present_mode: wgpu::PresentMode::Fifo,
+            present_mode: wgpu::PresentMode::Mailbox,
+        };
+        let swap_chain = device.create_swap_chain(&surface, &sc_desc);
+
+        let uniforms = Uniforms::new(size.width, size.height);
+
+        let uniform_buffer = device.create_buffer_init(
+            &wgpu::util::BufferInitDescriptor {
+                label: Some("Uniform Buffer"),
+                contents: bytemuck::cast_slice(&[uniforms]),
+                usage: wgpu::BufferUsage::UNIFORM | wgpu::BufferUsage::COPY_DST,
+            }
+        );
+
+        let uniform_bind_group_layout =
+            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                entries: &[wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStage::FRAGMENT,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                }],
+                label: Some("uniform_bind_group_layout"),
+            });
+
+        let uniform_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            layout: &uniform_bind_group_layout,
+            entries: &[wgpu::BindGroupEntry {
+                binding: 0,
+                resource: uniform_buffer.as_entire_binding(),
+            }],
+            label: Some("uniform_bind_group"),
+        });
+
+        let vs_module = device.create_shader_module(&wgpu::include_spirv!("shader.vert.spv"));
+        let fs_module = device.create_shader_module(&wgpu::include_spirv!("shader.frag.spv"));
+
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("Render Pipeline Layout"),
+                bind_group_layouts: &[
+                    &uniform_bind_group_layout,
+                ],
+                push_constant_ranges: &[],
+            });
+
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &vs_module,
+                entry_point: "main",
+                buffers: &[],
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &fs_module,
+                entry_point: "main",
+                targets: &[wgpu::ColorTargetState {
+                    format: sc_desc.format,
+                    alpha_blend: wgpu::BlendState::REPLACE,
+                    color_blend: wgpu::BlendState::REPLACE,
+                    write_mask: wgpu::ColorWrite::ALL,
+                }],
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: wgpu::CullMode::Back,
+                // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
+                polygon_mode: wgpu::PolygonMode::Fill,
+            },
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState {
+                count: 1,
+                mask: !0,
+                alpha_to_coverage_enabled: false,
+            },
+        });
+
+        Self {
+            surface,
+            device,
+            queue,
+            size,
+            sc_desc,
+            swap_chain,
+            render_pipeline,
+            uniforms,
+            uniform_buffer,
+            uniform_bind_group,
+        }
+    }
+
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        self.size = new_size;
+        self.sc_desc.width = new_size.width;
+        self.sc_desc.height = new_size.height;
+        self.swap_chain = self.device.create_swap_chain(&self.surface, &self.sc_desc);
+    }
+
+    #[allow(unused_variables)]
+    fn input(&mut self, event: &WindowEvent) -> bool {
+        false
+    }
+
+    fn update(&mut self, dt: std::time::Duration) {
+        self.uniforms.width = self.size.width;
+        self.uniforms.height = self.size.height;
+        self.uniforms.time += dt.as_secs_f32();
+        // write uniform buffer
+        self.queue.write_buffer(&self.uniform_buffer, 0, bytemuck::cast_slice(&[self.uniforms]));
+
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SwapChainError> {
+        let frame = self.swap_chain.get_current_frame()?.output;
+
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
+
+        {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[wgpu::RenderPassColorAttachmentDescriptor {
+                    attachment: &frame.view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: true,
+                    },
+                }],
+                depth_stencil_attachment: None,
+            });
+
+            render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.set_bind_group(0, &self.uniform_bind_group, &[]);
+            render_pass.draw(0..6, 0..1);
+        }
+
+        self.queue.submit(iter::once(encoder.finish()));
+
+        Ok(())
+    }
+}
+
+fn main() {
+    env_logger::init();
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_inner_size(winit::dpi::PhysicalSize::new(1600, 900))
+        .build(&event_loop).unwrap();
+
+    use futures::executor::block_on;
+
+    // Since main can't be async, we're going to need to block
+    let mut state = block_on(State::new(&window));
+
+    let mut last_render_time = std::time::Instant::now();
+    let mut frame_count = 0;
+    let mut frame_count_start = std::time::Instant::now();
+    event_loop.run(move |event, _, control_flow| {
+        match event {
+            Event::WindowEvent {
+                ref event,
+                window_id,
+            } if window_id == window.id() => {
+                if !state.input(event) {
+                    match event {
+                        WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                        WindowEvent::KeyboardInput { input, .. } => match input {
+                            KeyboardInput {
+                                state: ElementState::Pressed,
+                                virtual_keycode: Some(VirtualKeyCode::Escape),
+                                ..
+                            } => *control_flow = ControlFlow::Exit,
+                            _ => {}
+                        },
+                        WindowEvent::Resized(physical_size) => {
+                            state.resize(*physical_size);
+                        }
+                        WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                            // new_inner_size is &mut so w have to dereference it twice
+                            state.resize(**new_inner_size);
+                        }
+                        _ => {}
+                    }
+                }
+            }
+            Event::RedrawRequested(_) => {
+                let now = std::time::Instant::now();
+                let dt = now - last_render_time;
+                last_render_time = now;
+                state.update(dt);
+                match state.render() {
+                    Ok(_) => {}
+                    // Recreate the swap_chain if lost
+                    Err(wgpu::SwapChainError::Lost) => state.resize(state.size),
+                    // The system is out of memory, we should probably quit
+                    Err(wgpu::SwapChainError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                    // All other errors (Outdated, Timeout) should be resolved by the next frame
+                    Err(e) => eprintln!("{:?}", e),
+                }
+                // fps
+                frame_count = frame_count + 1;
+                let total_time = now - frame_count_start;
+                // refresh fps every 5 seconds
+                if total_time.as_secs_f32() > 3.0 {
+                    let fps = frame_count as f32 / total_time.as_secs_f32();
+                    println!("fps: {}", fps);
+                    frame_count = 0;
+                    frame_count_start = now;
+                }
+            }
+            Event::MainEventsCleared => {
+                // RedrawRequested will only trigger once, unless we manually
+                // request it.
+                window.request_redraw();
+            }
+            _ => {}
+        }
+    });
+}
---
+++ src/shader.frag
@@ -0,0 +1,284 @@
+#version 450
+
+layout(location=0) out vec4 f_color;
+
+layout(set=0, binding=0)
+uniform Uniforms {
+    int width;
+    int height;
+    float time;
+};
+
+const int NUM_STEPS = 128; // doesn't matter much right now
+const float MIN_DIST = 0.001; // threshold for intersection
+const float MAX_DIST = 1000.0; // oops we went into space
+
+
+// Struct that allows us to return more data about rays that we marched.
+struct Ray {
+    float d; 
+    float min_d; 
+    vec3 end; 
+    bool hit;
+};
+
+/* DISTANCE ESTIMATORS */    
+    
+// The distance estimator for a sphere
+float sdSphere(vec3 pos, vec3 spherePos, float size) {
+	return length(pos - spherePos) - size;
+}
+
+// Original from here: https://iquilezles.org/www/articles/distfunctions/distfunctions.htm
+float sdRoundBox(vec3 p, vec3 b, float r) {
+	vec3 q = abs(p) - b;
+	return length(max(q, 0.0)) + min(max(q.x, max(q.y, q.z)), 0.0) - r;
+}
+
+float sdPlane(vec3 p, vec3 n, float h)
+{
+    // n must be normalized
+    return dot(p,n) - h;
+}
+
+/* ROTATION */
+mat3 rotateX_matrix(float theta) {
+    // the -theta is cause the rotation matrices technically have to be reverse-rotation matrices
+    float c = cos(-theta);
+    float s = sin(-theta);
+
+    return mat3(
+        vec3(1, 0, 0),
+        vec3(0, c, s),
+        vec3(0, -s, c)
+    );
+}
+
+// vec3 rotateX(vec3 pos, float theta) {
+//     return (rotateX_matrix(-theta) * pos);
+// }
+
+mat3 rotateY_matrix(float theta) {
+    float c = cos(-theta);
+    float s = sin(-theta);
+
+    return mat3(
+        vec3(c, 0, -s),
+        vec3(0, 1, 0),
+        vec3(s, 0, c)
+    );
+}
+
+mat3 rotateZ_matrix(float theta) {
+    float c = cos(-theta);
+    float s = sin(-theta);
+
+    return mat3(
+        vec3(c, s, 0),
+        vec3(-s, c, 0),
+        vec3(0, 0, 1)
+    );
+}
+
+vec3 rotate(vec3 pos, vec3 center, mat3 matrix) {
+    return matrix * (pos - center) + center;
+}
+
+
+
+/* OPERATIONS */
+
+// a and b are interchangeable
+float opUnion(float a, float b) {
+    return min(a,b);
+}
+
+// a and b are NOT interchangeable
+float opDiff(float a, float b) {
+    return max(-a, b);
+}
+
+// a and b are interchangeable
+float opIntersect(float a, float b) {
+    return max(a,b);
+}
+
+vec3 opTranslate(vec3 pos, vec3 moves) {
+    return pos - moves;
+}
+
+/* RENDERING */
+
+mat3 rot_X = rotateX_matrix(time);
+mat3 rot_Y = rotateY_matrix(time/4);
+mat3 rotation_main = rot_X * rot_Y;
+
+float scene_floorless(vec3 pos) {
+    float d = 1e10;
+
+    // box-sphere difference
+    vec3 center = vec3(0, 1.5, 0);
+    vec3 pos_rotated = rotate(pos, center, rotation_main);
+    d = min(d, 
+        opIntersect(
+            sdSphere(pos_rotated, center, 1.35),
+            opDiff(
+                sdSphere(pos_rotated, center, 1.25),
+                sdRoundBox(opTranslate(pos_rotated, center), vec3(1.0,1.0,1.0), 0.0)
+            )
+        )
+    );
+
+    d = min(d, sdSphere(pos, vec3(2, 0.5, -2), 0.5));
+    d = min(d, sdSphere(pos, vec3(4, 1.0, 3), 1.0));
+    d = min(d, sdSphere(pos, vec3(-3, 1.5, 2), 1.5));
+    d = min(d, sdSphere(pos, vec3(-4, 1.0, 0), 1.0));
+    d = min(d, sdSphere(pos, vec3(-2, 0.5, -1), 0.5));
+    d = min(d, sdSphere(pos, vec3(-2, 0.25, -2.25), 0.25));
+
+    return d;
+    // return sdSphere(pos, vec3(1.0, 0.5, 0.0), 1.0);
+}
+
+float scene(vec3 pos) {
+    return min(scene_floorless(pos), sdPlane(pos, vec3(0.0, 1.0, 0.0), 0));
+}
+
+// vec3 getNormal(in vec3 p) {
+// 	const vec2 e = vec2(.0001, 0);
+// 	return normalize(vec3(scene(p + e.xyy) - scene(p - e.xyy), scene(p + e.yxy) - scene(p - e.yxy),	scene(p + e.yyx) - scene(p - e.yyx)));
+// }
+
+// https://iquilezles.org/www/articles/normalsSDF/normalsSDF.htm
+vec3 getNormal(vec3 p)
+{
+    const float h = 0.0001; // replace by an appropriate value
+    const vec2 k = vec2(1,-1);
+    return normalize(k.xyy * scene(p + k.xyy*h) + 
+                     k.yyx * scene(p + k.yyx*h) + 
+                     k.yxy * scene(p + k.yxy*h) + 
+                     k.xxx * scene(p + k.xxx*h));
+}
+
+vec2 hash32(vec3 p3)
+{
+	p3 = fract(p3 * vec3(.1031, .1030, .0973));
+    p3 += dot(p3, p3.yzx+33.33);
+    return fract((p3.xx+p3.yz)*p3.zy);
+}
+
+uvec3 pcg3d(uvec3 v) {
+
+    v = v * 1664525u + 1013904223u;
+
+    v.x += v.y*v.z;
+    v.y += v.z*v.x;
+    v.z += v.x*v.y;
+
+    v ^= v >> 16u;
+
+    v.x += v.y*v.z;
+    v.y += v.z*v.x;
+    v.z += v.x*v.y;
+
+    return v;
+}
+
+vec3 hash23alt(vec2 s){
+    s = 353.0*s;
+    uvec4 u = uvec4(s, uint(s.x) ^ uint(s.y), uint(s.x) + uint(s.y));
+    return vec3(pcg3d(u.xyz)) * (1.0/float(0xffffffffu));
+}
+
+Ray march(vec3 origin, vec3 direction) {
+    float t = 0.0;
+    float d_min = MAX_DIST;
+    float t_max = MAX_DIST;
+
+    // raytrace floor plane
+    float t_floor = -origin.y / direction.y;
+    if (t_floor > 0.0){
+        // we should never trace further than to the floor
+        t_max = min(t_max, t_floor);
+    }
+    
+    int i = 0;
+    for(; i < NUM_STEPS; i++) {
+        vec3 current_pos = origin + t * direction;
+        
+        // Use our distance estimator to find the distance
+        float d = scene_floorless(current_pos);
+        
+        // keep track of the smallest step we've had
+        d_min = d < d_min ? d : d_min;
+        
+        // Check if we hit something or have gone too far
+        if (d < MIN_DIST || t > t_max) {
+            break;
+        }
+
+        // Add the marched distance to total
+        t += d;
+    }
+
+    vec3 pos = origin + t * direction;
+    bool finished = (i == NUM_STEPS || t > t_max);
+    // did we hit something?
+    if (!finished) {
+        return Ray(t, d_min, pos, true);
+    }
+    // did we hit the floor?
+    if (t_floor > 0.0 && finished) {
+        return Ray(t_floor, d_min, origin + t_floor * direction, true);
+    }
+    // else we missed :(
+    return Ray(MAX_DIST, d_min, pos, false);
+    
+}
+
+void main()
+{
+    vec2 iResolution = vec2(float(width), float(height));
+    float iTime = time;
+
+    int num_levels = 8;
+    vec3 color = vec3(0.0);
+    for (int i = 0; i < num_levels; i++) {
+        // Normalized pixel coordinates (from 0 to 1)
+        // vec2 offset = hash32(vec3(gl_FragCoord.xy, fract(iTime) + float(i) / num_levels));
+        vec2 offset = hash23alt(gl_FragCoord.xy + iTime + float(i) / num_levels).xy;
+        vec2 uv = ((gl_FragCoord.xy + offset) / iResolution.xy) * 2.0 - 1.0;
+        uv.y *= -1.0; // flip the y-axis
+        
+        // this makes sure that the [-1, 1] range is always on the screen regardless of the aspect ratio
+        // so in a landscape 16:9 aspect ratio uv.x would be in the range [-1.77, 1.77] and uv.y would be in the range [-1, 1]
+        if (iResolution.x > iResolution.y) {
+            uv.x = uv.x * (iResolution.x / iResolution.y);
+        } else {
+            uv.y = uv.y * (iResolution.y / iResolution.x);
+        }
+        // f_color = vec4(mod(uv.x, 1.0), mod(uv.y, 1.0), 0.0, 1.0);
+        
+        // camera Y-rotation angle in radians
+        // float cam_rot_y = time / 8;
+
+        vec3 camPos = vec3(0.0, 2.5, -8.0);
+        // vec3 rayDir = normalize(vec3(uv, 1.0));
+        // this fixes the horizontal fov to 90 degrees
+        float max_u = iResolution.x / iResolution.y;
+        vec3 rayDir = normalize(vec3(uv, max_u));
+        
+        Ray marched = march(camPos, rayDir);
+
+        vec3 normal = getNormal(marched.end);
+        
+        color += marched.hit ? 
+            // vec3(0.5,1.0,1.0) * vec3(marched.d/pow(float(NUM_STEPS), 0.8) * 4.0) : 
+            // vec3(0.0,1.0,1.0) * vec3(pow(clamp(-1.0 * marched.min_d + 1.0, 0.0, 1.0), 4.0) / 2.0);
+            // vec3(0.5, 1.0, 1.0) * 1 / marched.d :
+            (normal.xzy + 1) / 2 : //the .xzy swizzle is just to make the colors prettier :p
+            vec3(0.0, 0.0, 0.0);
+    }
+    
+    f_color = vec4(color / num_levels, 1.0);
+}
